2b68b34a6f009d57a2799bc04ce7b06d47308a50
Ikbal Čaušević <ikbalcaus@gmail.com>
2024-10-30 15:46:13 +0100

    Added erasing gesture

--- main.py
+++ main.py
@@ -11,32 +11,64 @@ height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 canvas = np.ones((height, width, 3), dtype="uint8") * 255
 
 prev_x, prev_y = None, None
+lines = []
 
 with mp_hand.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
     while True:
         ret, frame = video.read()
-
         frame = cv2.flip(frame, 1)
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         results = hands.process(rgb_frame)
 
         if results.multi_hand_landmarks:
-            for hand_landmark in results.multi_hand_landmarks:
-                mp_draw.draw_landmarks(frame, hand_landmark, mp_hand.HAND_CONNECTIONS)
+            hand_landmark = results.multi_hand_landmarks[0]
+            mp_draw.draw_landmarks(frame, hand_landmark, mp_hand.HAND_CONNECTIONS)
 
-                x_index = int(hand_landmark.landmark[8].x * frame.shape[1])
-                y_index = int(hand_landmark.landmark[8].y * frame.shape[0])
+            landmark_7_vertical = int(hand_landmark.landmark[7].y * frame.shape[0])
+            landmark_8_horizontal = int(hand_landmark.landmark[8].x * frame.shape[1])
+            landmark_8_vertical = int(hand_landmark.landmark[8].y * frame.shape[0])
 
+            landmark_4_vertical = int(hand_landmark.landmark[4].y * frame.shape[0])
+            landmark_11_vertical = int(hand_landmark.landmark[11].y * frame.shape[0])
+            landmark_12_vertical = int(hand_landmark.landmark[12].y * frame.shape[0])
+            landmark_15_vertical = int(hand_landmark.landmark[15].y * frame.shape[0])
+            landmark_16_vertical = int(hand_landmark.landmark[16].y * frame.shape[0])
+            landmark_19_vertical = int(hand_landmark.landmark[19].y * frame.shape[0])
+            landmark_20_vertical = int(hand_landmark.landmark[20].y * frame.shape[0])
+
+            index_finger_extended = (
+                landmark_4_vertical < landmark_12_vertical and
+                landmark_8_vertical < landmark_11_vertical
+            )
+            fingers_extended = (
+                landmark_8_vertical < landmark_7_vertical and
+                landmark_12_vertical < landmark_11_vertical and
+                landmark_16_vertical < landmark_15_vertical and
+                landmark_20_vertical < landmark_19_vertical
+            )
+
+            if index_finger_extended:
                 if prev_x is not None and prev_y is not None:
-                    cv2.line(canvas, (prev_x, prev_y), (x_index, y_index), (0, 0, 0), 5)
+                    cv2.line(canvas, (prev_x, prev_y), (landmark_8_horizontal, landmark_8_vertical), (0, 0, 0), 5)
+                    lines.append(((prev_x, prev_y), (landmark_8_horizontal, landmark_8_vertical)))
+                prev_x, prev_y = landmark_8_horizontal, landmark_8_vertical
+            else:
+                prev_x, prev_y = None, None
 
-                prev_x, prev_y = x_index, y_index
+            if fingers_extended:
+                if lines:
+                    lines.pop()
+                    canvas = np.ones((height, width, 3), dtype="uint8") * 255
+                    for line in lines:
+                        cv2.line(canvas, line[0], line[1], (0, 0, 0), 5)
+
+            cv2.circle(frame, (landmark_8_horizontal, landmark_8_vertical), 5, (200, 200, 200), -1)
 
         else:
             prev_x, prev_y = None, None
 
-        cv2.imshow("Drawing Canvas", canvas)
+        cv2.imshow("Canvas", canvas)
         cv2.imshow("Frame", frame)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
